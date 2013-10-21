7392e010b3246aac264211c765df6c1899e87657
Jason Leveille <jason@leve.us>
2013-10-21 10:49:05 -0400

    Update version parser

--- opengraph/__init__.py
+++ opengraph/__init__.py
@@ -2,6 +2,6 @@ __all__ = ('VERSION',)
 
 try:
     VERSION = __import__('pkg_resources') \
-        .get_distribution('django-twittercard').version
+        .get_distribution('django-opengraph').version
 except Exception, e:
     VERSION = 'unknown'
